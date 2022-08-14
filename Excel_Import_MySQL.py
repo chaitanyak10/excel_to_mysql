718fae4c67ce750b7afb0ead60872380b2de4e02
Chaitanya Kulkarni <chaitanya.ukulkarni@gmail.com>
2022-08-14 15:51:45 +0530

    This is my Python code to bulk load excel data in mysql

---
+++ Excel_Import_MySQL.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pymysql
+import sqlite3
+import cryptography
+
+pymysql.install_as_MySQLdb()
+from sqlalchemy import create_engine
+
+df = pd.read_excel("D:\Chaitanya Kulkarni\Python iNeuron Class\data fsds -20220813T092523Z-001\data fsds\Attribute "
+                   "DataSet.xlsx")
+
+conn = sqlite3.connect('mydatabase')
+
+engine = create_engine('mysql://localhost:Password@localhost/mydatabase')
+
+df.to_sql('people', con=engine, if_exists='append', index=False)
